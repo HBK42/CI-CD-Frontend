bd70fc13c0ff5d2172f88617a7e8044fcee7b356
HBK42 <hasankizilirmak42@gmail.com>
2024-06-20 21:58:30 +0200

    Update eslint.config.js

--- cicd-frontend-main/eslint.config.js
+++ cicd-frontend-main/eslint.config.js
@@ -1,26 +1,17 @@
-import vue from 'eslint-plugin-vue';
-
-export default [
-  {
-    files: ['**/*.js', '**/*.vue'],
-    languageOptions: {
-      ecmaVersion: 2021,
-      sourceType: 'module',
-      globals: {
-        axios: true,
-        vue: true,
-      },
-    },
-    plugins: {
-      vue,
-    },
-    processor: 'vue/.vue',
-    extends: [
-      'eslint:recommended',
-      'plugin:vue/vue3-recommended',
-    ],
-    rules: {
-      // Fügen Sie benutzerdefinierte Regeln hinzu, falls benötigt
-    },
+module.exports = {
+  root: true,
+  env: {
+    node: true,
   },
-];
+  extends: [
+    'plugin:vue/essential',
+    'eslint:recommended',
+  ],
+  parserOptions: {
+    parser: 'babel-eslint',
+  },
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+  },
+};
