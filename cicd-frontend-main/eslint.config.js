a9d9a2361fbc911f7a190df5d312ccb314d332b0
HBK42 <hasankizilirmak42@gmail.com>
2024-06-20 18:02:32 +0200

    Update eslint.config.js

--- cicd-frontend-main/eslint.config.js
+++ cicd-frontend-main/eslint.config.js
@@ -1,15 +1,19 @@
 module.exports = {
-  env: {
-    node: true,
+  languageOptions: {
+    globals: {
+      // Define your global variables here, if any
+      // For example:
+      process: true,
+      // Add other global variables as needed
+    },
+    ecmaVersion: 2020, // Specify the ECMAScript version
+    sourceType: 'module', // Specify the source type
   },
   extends: [
     'eslint:recommended',
     'plugin:vue/vue3-essential',
     'plugin:react/recommended',
   ],
-  parserOptions: {
-    ecmaVersion: 2020,
-  },
   rules: {
     // Add your custom rules here
   },
