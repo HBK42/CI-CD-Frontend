e9fbc45385e146a5e0fe6fb92f03b5df3c770460
HBK42 <hasankizilirmak42@gmail.com>
2024-06-20 17:21:53 +0200

    .

--- cicd-frontend-main/eslint.config.mjs
+++ cicd-frontend-main/eslint.config.mjs
@@ -1,22 +1,17 @@
-// eslint.config.js
-import vue from 'eslint-plugin-vue';
-
-export default [
-  {
-    files: ['**/*.js', '**/*.vue'],
-    languageOptions: {
-      ecmaVersion: 'latest',
-      sourceType: 'module',
-    },
-    plugins: {
-      vue,
-    },
-    extends: [
-      'eslint:recommended',
-      'plugin:vue/essential',
-    ],
-    rules: {
-      // Add custom rules here
-    },
+module.exports = {
+  root: true,
+  env: {
+    node: true,
   },
-];
+  extends: [
+    'eslint:recommended',
+    'plugin:vue/vue3-essential',
+    'plugin:react/recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 2020,
+  },
+  rules: {
+    // Add your custom rules here
+  },
+};
