ac8056cfdd36044e886cd94eee2db1af540eda2d
HBK42 <hasankizilirmak42@gmail.com>
2024-06-20 19:11:48 +0200

    Update eslint.config.js

--- cicd-frontend-main/eslint.config.js
+++ cicd-frontend-main/eslint.config.js
@@ -1,13 +1,23 @@
-const { FlatCompat } = require("@eslint/eslintrc");
-const path = require("path");
-
-// Mimic CommonJS variables
-const __dirname = path.dirname(new URL(import.meta.url).pathname);
-
-const compat = new FlatCompat({
-    baseDirectory: __dirname
-});
-
-module.exports = [
-    ...compat.extends("eslint-config-my-config"),
-];
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:vue/vue3-essential', // Extends Vue 3 essential rules
+  ],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest', // Use the latest ECMAScript version
+    sourceType: 'module',
+  },
+  plugins: [
+    'vue', // Enables ESLint plugin for Vue
+  ],
+  rules: {
+    'vue/multi-word-component-names': 'off', // Disables the rule that enforces multi-word component names in Vue
+    // Add more rules or override rules as needed
+  },
+};
